04959280d89c0cbe05efcb4081fc107ff8d33c5b
SteveM <SteveM@myEtherWallet.com>
2019-07-31 13:41:46 -0700

    clean2

--- src/nft/contracts/index.js
+++ src/nft/contracts/index.js
@@ -1,7 +1,5 @@
 import cryptoKitties from './cryproKitties'
 import nonStandardContracts from './nonStandardContracts'
-import mycryptoheroes from './mycryptoheroes'
-import godsunchained from './godsunchained'
 import staticmetadata from './StaticMetadata'
 import metadata from './721Metadata'
 
@@ -9,8 +7,6 @@ import metadata from './721Metadata'
 export {
   cryptoKitties,
   nonStandardContracts,
-  mycryptoheroes,
-  godsunchained,
   staticmetadata,
   metadata
 }
