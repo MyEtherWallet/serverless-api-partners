4d21ad0c44191ccc6aa492c8e97346bb69349199
SteveM <SteveM@myEtherWallet.com>
2020-03-18 16:12:23 -0700

    changelly hotfix to prevent error pop-up

--- src/changelly/index.js
+++ src/changelly/index.js
@@ -1,9 +1,12 @@
-import { error, success } from "../response";
-import changellyConfigs from "./config";
-import allowedMethods from "./validMethods";
-import request from "../request";
-import crypto from "crypto";
+import {error, success} from '../response';
+import changellyConfigs from './config';
+import allowedMethods from './validMethods';
+import request from '../request';
+import crypto from 'crypto';
+
 export default (req, logger) => {
+  let alternativeHandlingMethods = ['getFixRate', 'getExchangeAmount'];
+  let activeMethod = false; // getFixRate getExchangeAmount
   return new Promise((resolve, reject) => {
     if (req.body) {
       let body = req.body;
@@ -14,24 +17,37 @@ export default (req, logger) => {
         if (allowedMethods.indexOf(body.method) === -1)
           reject(error(`Invalid Method - ${body.method}`));
         else {
+          activeMethod = alternativeHandlingMethods.includes(body.method);
           const req = {
             url: changellyConfigs.API_URL,
             headers: {
-              "api-key": changellyConfigs.CHANGELLY_API_KEY,
+              'api-key': changellyConfigs.CHANGELLY_API_KEY,
               sign: crypto
-                .createHmac("sha512", changellyConfigs.CHANGELLY_SECRET)
+                .createHmac('sha512', changellyConfigs.CHANGELLY_SECRET)
                 .update(JSON.stringify(body))
-                .digest("hex")
+                .digest('hex')
             }
           };
           request(req, body)
             .then(result => {
+              if(!Array.isArray(result)){
+                if(result.error){
+                  if (logger) console.log('CHANGELLY ERROR', result);
+                  resolve(success({result: [{
+                    min: 0,
+                    max: 0,
+                    result: 0,
+                    id: 0
+                  }]}));
+                  return;
+                }
+              }
               resolve(success(result));
             })
             .catch(err => {
-              if(logger) console.log('CHANGELLY ERROR', error);
-              if(logger) logger.errorReporter('changelly');
-              reject(error(err, ""));
+              if (logger) console.log('CHANGELLY ERROR', err);
+              if (logger) logger.errorReporter('changelly');
+              reject(error(err, ''));
             });
         }
       }
