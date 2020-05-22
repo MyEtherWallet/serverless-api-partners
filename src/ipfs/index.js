f8dd6c9e91899ab2445ca10165405a7d260d4d58
gamalielhere <gamalielgad1234@gmail.com>
2020-05-21 21:20:56 -0700

    Make unzipping, and upload work

--- src/ipfs/index.js
+++ src/ipfs/index.js
@@ -6,8 +6,9 @@ import AWS from 'aws-sdk';
 import fs from 'fs';
 import IpfsHttpClient from 'ipfs-http-client';
 import fetch from 'node-fetch';
+import contentHash from 'content-hash';
 const { globSource } = IpfsHttpClient;
-const PATH = '/tmp';
+const PATH = './tmp/';
 AWS.config.update({ region: ipfsConfig.REGION || 'us-east-2' })
 const s3 = new AWS.S3({
   signatureVersion: 'v4'
@@ -26,23 +27,33 @@ function loginToTemporal(usr, pw) {
   })
 }
 
-async function uploadToIpfs(resolve, reject, token, file) {
-  // unzip file
-  fs.mkdirSync(PATH);
+async function uploadToIpfs(resolve, reject, token, file, hash) {
+  // unzip file 
+  if(!fs.existsSync(PATH)) {
+    fs.mkdirSync(`${PATH}/${hash}`);
+  } else {
+    // fs.rmdirSync(PATH);
+    fs.mkdirSync(`${PATH}/${hash}`, {recursive: true});
+  }
   const unzipped = new AdmZip(file);
-  unzipped.extractAllTo(PATH);
-
+  unzipped.extractAllTo(`${PATH}/${hash}`, true);
   const ipfs = IpfsHttpClient({
-    host: ipfsConfig.API_UPLOAD_URL,
+    host: ipfsConfig.API_UPLOAD_HOST,
+    port: ipfsConfig.API_UPLOAD_PORT,
+    protocol: ipfsConfig.API_UPLOAD_PROTOCOL,
     headers: {
-      authorization: `Bearer ${token}`
+      authorization: `Bearer ${token.token}`
     }
   });
-
+  
   try {
-    for await (const file of ipfs.add(globSource('./docs', { recursive: true }))) {
-      if(file.path === PATH.replace('/', '')) {
-        resolve(success(file.cid));
+    const folderName = fs.readdirSync(`${PATH}/${hash}`);
+    const file = await ipfs.add(globSource(`${PATH}/${hash}/${folderName[0]}`, {recursive: true}));
+    for await (const f of file) {
+      if(file.path === folderName[0]) {
+        const actualCid = file.cid.substring(4, file.cid.length -1);
+        const hash = contentHash.fromIpfs(actualCid);
+        resolve(success(hash));
       }
     }
   } catch(e) {
@@ -63,35 +74,39 @@ export default (req) => {
         const signedUrl = s3.getSignedUrl('putObject', s3Params);
         resolve(
           success({
-            "statusCode": 200,
-            "body": JSON.stringify({
+            "body": {
               "signedUrl": signedUrl,
               "hashResponse": hash
-            })
+            }
           })
         );
       } else if (req.body.method === ipfsConfig.UPLOAD_COMPLETE) {
         const fileHash = req.body.hash;
         const s3Params = {
           Bucket: ipfsConfig.BUCKET_NAME,
-          Key:  `${fileHash}.zip`
+          Key: `${fileHash}.zip`
         }
         // get file from s3
-        const signedUrl = s3.getSignedUrl('getObject', s3Params);
-        fetch(signedUrl).then(retrievedFile => {
-          try {
-            // login to temporal
-            const tokenPromise = loginToTemporal(ipfsConfig.TEMPORAL_USERNAME, ipfsConfig.TEMPORAL_PW).then(tempLogin => {
-              // upload files to ipfs
-              return tempLogin.json();
-            });
-            tokenPromise.then(token => {
-              uploadToIpfs(resolve, reject, token, retrievedFile);
-            }).catch(reject(error("Error fetching temporal token")))
-          } catch(e) {
-            reject(error(e));
-          }
-        })
+        s3.getObject(s3Params)
+        .promise()
+        .then(data => {
+            try {
+              // login to temporal
+              const tokenPromise = loginToTemporal(ipfsConfig.TEMPORAL_USERNAME, ipfsConfig.TEMPORAL_PW).then(tempLogin => {
+                // upload files to ipfs
+                return tempLogin.json();
+              });
+              tokenPromise.then(token => {
+                uploadToIpfs(resolve, reject, token, data.Body);
+              }).catch(e => {
+                reject(error(e))
+              })
+            } catch(e) {
+              reject(error(e));
+            }
+        }).catch(e => {
+                reject(error(e))
+              });
       } else {
         reject(error("Can't understand API call"))
       }
---
+++ test.js
@@ -0,0 +1,13 @@
+// Way to test functions locally
+
+// AWS_PROFILE=claudia babel-node test.js
+
+// EXAMPLE
+// import ipfs from './src/ipfs/index.js';
+
+// ipfs({
+//   "body": {
+//       "method": "uploadComplete",
+//       "hash": "77d13abf-7c73-4479-a18e-4c4b7c7c6944"
+//     }
+// }).then(console.log)
